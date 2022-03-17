fe2f19b534d02aa7309dbbc4e0ae551d8a80f41a
deoatanta <armadadeo064@gmail.com>
2022-03-17 19:59:53 +0700

    Pembersihan file kode yang tidak diperlukan

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/mahasiswa','mhsController@index');
-Route::get('/myprofil','mhsController@profil');
-Route::get('/portfolio','mhsController@pf');
-Route::get('/dosen', 'dosenController@index');
-Route::redirect('/infomhs','mahasiswa');
-
-route::get('/dataMhs','mhsController@index');
-
 Auth::routes();
-// var_dump(Auth::users());
-// die();
-// Auth::check();
 
-// Auth();
 Route::resource('/prediction',PredictionController::class);
 Route::resource('/analytic',analyticController::class);
 Route::get('/home', 'HomeController@index')->name('home');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
